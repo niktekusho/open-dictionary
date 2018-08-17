1ca6811bd581987215199438abb96835f7c6949f
Nicola <nicola.dalmaso@outlook.com>
2018-08-17 17:29:54 +0200

    Improved queries test coverage

--- src/users/repository/queries.spec.js
+++ src/users/repository/queries.spec.js
@@ -5,8 +5,39 @@ describe('User repository -> \'Available queries\' test suite', () => {
 		it('exported object should be a function', () => {
 			expect(queries).toEqual(expect.any(Function));
 		});
+		it('function invocation should return the available queries', () => {
+			const availableQueries = queries();
+			expect(availableQueries).toHaveProperty('equalsUsername');
+			expect(availableQueries).toHaveProperty('equalsEmail');
+			expect(availableQueries).toHaveProperty('createdToday');
+		});
 	});
 
 	describe('Evaluating module functionality', () => {
+		describe('Evaluating "equalsUsername"', () => {
+			it ('should create an username object', () => {
+				const availableQueries = queries();
+				expect(availableQueries.equalsUsername('test')).toEqual({username: 'test'});
+			});
+		});
+		describe('Evaluating "equalsEmail"', () => {
+			it ('should create an email object', () => {
+				const availableQueries = queries();
+				expect(availableQueries.equalsEmail('test')).toEqual({email: 'test'});
+			});
+		});
+		describe('Evaluating "createdToday"', () => {
+			it ('should create an _id query object', () => {
+				// Mocked mongodb dependency
+				const mongodb = {
+					ObjectID: {
+						createFromTime: jest.fn(() => 'sometime')
+					}
+				};
+				const availableQueries = queries(mongodb);
+				expect(availableQueries.createdToday()).toEqual({_id: { $gte: 'sometime'}});
+				expect(mongodb.ObjectID.createFromTime).toHaveBeenCalledTimes(1);
+			});
+		});
 	});
 });
