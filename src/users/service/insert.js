8bb70dc71b1722be5ce9e9aaad7c885e72f681c4
Nicola <nicola.dalmaso@outlook.com>
2018-08-21 10:46:09 +0200

    Improved user service insert + added unit test

--- src/users/service/insert.js
+++ src/users/service/insert.js
@@ -1,8 +1,20 @@
-// const {validateUser} = require('./validation.utils');
+const {isValidUser} = require('./validation.utils');
 
-// module.exports = async (user, userRepository, logger) => {
-// 	if (user) {
-
-// 	}
-// 	throw new Error('Inserting undefined user');
-// }
+module.exports = async (user, userRepository, logger) => {
+	if (user) {
+		const validationResult = isValidUser(user);
+		if (validationResult.valid) {
+			try {
+				logger.debug('User service -> Insert -> Valid user passed in');
+				const repositoryResult = await userRepository.insert(user);
+				return repositoryResult;
+			} catch (err) {
+				logger.debug('User service -> Insert -> Invalid user passed in');
+				throw new Error(`Unexpected repository error.\n${err}`);
+			}
+		}
+		throw new Error(`User validation failed:\n${validationResult.errors}`);
+	}
+	logger.debug('User service -> Insert -> Undefined user passed in');
+	throw new Error('Inserting undefined user');
+};
