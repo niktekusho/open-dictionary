aee236846c132a1b970ed5f279ab82128e87dd6b
Nicola <nicola.dalmaso@outlook.com>
2018-08-23 15:30:03 +0200

    User naming convention alignment

--- src/users/repository/db-validation-schema.spec.js
+++ src/users/repository/db-validation-schema.spec.js
@@ -1,15 +1,15 @@
-const validationSchema = require('./validation.schema');
+const validationSchema = require('./validation-schema');
 
 describe('User repository -> \'Validation schema\' test suite', () => {
-	describe('Evaluating exported module', () => {
-		it('exported object should be an object', () => {
-			expect(validationSchema).toEqual(expect.any(Object));
-		});
-	});
+    describe('Evaluating exported module', () => {
+        it('exported object should be an object', () => {
+            expect(validationSchema).toEqual(expect.any(Object));
+        });
+    });
 
-	describe('Evaluating module functionality', () => {
-		it('should contain the \'$jsonSchema\' property', async () => {
-			expect(validationSchema).toHaveProperty('$jsonSchema');
-		});
-	});
+    describe('Evaluating module functionality', () => {
+        it('should contain the \'$jsonSchema\' property', async() => {
+            expect(validationSchema).toHaveProperty('$jsonSchema');
+        });
+    });
 });
