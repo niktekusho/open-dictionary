d12303293b1ce9c3d0943dab92a2716c41ac4fa7
Nicola Dal Maso <niktekusho@users.noreply.github.com>
2018-08-28 16:32:46 +0200

    Fixes broken npm scripts (#7)
    * Enable docker service on Travis
    * Custom integration script...
    * Modified package scripts in order to run integration tests
    * Pulling mongo image since it seems the integration test script cannot reach docker hub
    * YAML specification wants spaces (not tabs)...
    * Disabled integration tests on osx since Travis disables services on osx
    * Added debug line to catch if Travis is pulling the mongo image
    * Moving docker pull inside build matrix
    * Fixing silly goose mistakes...
    * Cleaning up
    * Fix #5

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -3,4 +3,7 @@ const config = require('./jest.config');
 // Change the test pattern
 config.testRegex = 'ispec.js$';
 
+// Set 10 seconds of timeout (Travis being slow...)
+config.setupTestFrameworkScriptFile = './jest.setup.js';
+
 module.exports = config;
