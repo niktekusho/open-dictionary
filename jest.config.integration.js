5839a3efc2c95dd0210fa1a70dcfba42931c8457
Nicola <nicola.dalmaso@outlook.com>
2018-08-28 11:20:03 +0200

    ...

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -3,4 +3,7 @@ const config = require('./jest.config');
 // Change the test pattern
 config.testRegex = 'ispec.js$';
 
+// Set 10 seconds of timeout (Travis being slow...)
+jest.setTimeout(10 * 1000);
+
 module.exports = config;
