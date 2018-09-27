07f354ece8f3071dbd1eb97023bb720b2d72e392
Nicola <nicola.dalmaso@outlook.com>
2018-09-27 10:08:11 +0200

    Fixed linting errors

--- src/app/app-users.js
+++ src/app/app-users.js
@@ -5,7 +5,7 @@ const userAPI = require('../users/user-api');
 async function plugin(fastify, opts, next) {
 	const {userService} = fastify;
 	if (userService === null || userService === undefined) {
-		throw new Error('User Service must be initialized before the application starts.')
+		throw new Error('User Service must be initialized before the application starts.');
 	}
 	const usersRoutes = userAPI(userService);
 	fastify.register(usersRoutes, {prefix: '/users'});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const mongodb = require('mongodb');
 const fastify = require('fastify');
 
 const usersSetupPlugin = require('./app/app-setup');
@@ -12,12 +11,13 @@ app
 	.register(usersSetupPlugin)
 	.register(usersAPIPlugin);
 
-app.ready(async (err) => {
+app.ready(async err => {
 	if (err) {
 		console.error(`Could not boot server: ${err.message}`);
 		console.debug(err.stack);
 		console.info('Aborting startup...');
 		process.kill(process.pid, 'SIGTERM');
+		// eslint-disable-next-line unicorn/no-process-exit
 		process.exit(1);
 	}
 	const port = 3000;
